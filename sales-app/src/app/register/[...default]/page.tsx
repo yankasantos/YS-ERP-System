e6e7be787c5aefd23eaabe83436b6c76ea58640a
yankasantos <yankasantos0107@gmail.com>
2025-02-02 16:44:16 -0300

    working with router dynamic on nextJS

---
+++ sales-app/src/app/register/[...default]/page.tsx
@@ -0,0 +1,12 @@
+export function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  return <div>Page inside register module by ID {id}</div>;
+}
